c8dbbbc0e336e4eb7efdc68b9890d855334d12b3
Tennyson Onovwiona <ovie.tennyson@outlook.com>
2021-02-20 20:44:53 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req,res) => {
-    res.send('<h1>The beginningmmm of Resume genrator me!</h1>')
+    res.send('<h1>The beginningjghjggmmm of Resume genrator me!</h1>')
 })
 
 app.listen(3000)
