7a4dac65a328beb9eb0c8a107ac4a234e7d72931
Ovie Tennyson <atomictenny@gmail.com>
2021-02-21 16:19:11 +0100

    Initial Git Commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req,res) => {
-    res.send('<h1>The Resume genrator me!</h1>')
+    res.send('<h1>The Resume genrator for me!</h1>')
 })
 
 app.listen(3000)
