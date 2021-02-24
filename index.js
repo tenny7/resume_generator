610ce4cf46d4fd880edf294cc58c0da42241058f
Ovie Tennyson <atomictenny@gmail.com>
2021-02-24 10:26:17 +0000

    local commit

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ app.get('/', (req,res) => {
     res.send('<h1>The beginning of Resume genrator!</h1>')
 })
 
+var options = {
+
+    index: 'index.js'
+    
+    };
+
 app.use('/', express.static('/home/site/wwwroot', options));
 
 app.listen(process.env.PORT)
