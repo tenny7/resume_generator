e4106986d9177232d107810f10f5d85991960633
Tennyson Onovwiona <ovie.tennyson@outlook.com>
2021-02-24 08:42:23 +0000

    Updated server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req,res) => {
-    res.render('index')
+    res.send('<h1>The beginning of me Resume genrator!</h1>')
 })
 
 app.listen(3000)
