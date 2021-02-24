2bc1c6584f20f974bb3c0da8fbf2504d431b4906
Ovie Tennyson <atomictenny@gmail.com>
2021-02-24 08:46:55 +0000

    local changes to remote

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req,res) => {
-    res.send('<h1>The beginning of me Resume genrator!</h1>')
+    res.send('<h1>The beginning of Resume genrator!</h1>')
 })
 
 app.listen(3000)
