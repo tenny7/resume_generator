ba30c49151926f9a789669f385530a4f5a95c10f
Ovie Tennyson <atomictenny@gmail.com>
2021-02-26 08:39:12 +0000

    Added Jade server

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+
 app.get('/', (req,res) => {
-    res.render('index')
+    res.send('index')
 })
 
 var options = {
