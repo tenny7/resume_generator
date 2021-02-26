c96ea803887193d1e545ccfe7c5db80c69e67d96
Ovie Tennyson <atomictenny@gmail.com>
2021-02-26 06:50:36 +0000

    Added port for server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req,res) => {
-    res.send('<h1>The beginning of Resume genrator!</h1>')
+    res.send('The beginning of Resume genrator!')
 })
 
 // var options = {
