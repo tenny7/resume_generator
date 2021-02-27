f7719be9d96df3c92b4a5940e6b596193fa53bd2
Ovie Tennyson <atomictenny@gmail.com>
2021-02-27 12:56:58 +0000

    Modified View Engine, reverted to HTML

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const express = require('express')
 const path = require("path");
-
 const app = express()
+const base_path = path.join(__dirname, 'views/')
+
 const router = express.Router()
 const port = process.env.PORT || 3000
 
-app.set('view engine', 'pug')
+app.set('View engine', 'html')
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+
 app.set("views", path.join(__dirname, "views"));
 
-console.log(__dirname, "views");
-
 app.get('/', (req,res) => {
-    res.render('index')
+    res.sendFile(base_path +'index.html')
 })
 
 app.get('/layout', (req,res) => {
-    res.render('layout')
+    res.sendFile(base_path +'layout.html')
 })
 
 app.use('/', router)
