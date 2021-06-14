c82f9bc29ea2c9b61fb811a687885c3c5bf16d7d
Ovie Tennyson <atomictenny@gmail.com>
2021-06-14 20:35:56 +0000

    Modified code structure. Linked the model, controller for the routes

--- controllers/indexController.js
+++
@@ -1,5 +0,0 @@
-var Index =  require('../models/index')
-
-exports.index = (req,res) => {
-    res.send('Home cotroller')
-}
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 const express = require('express')
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 const path = require("path");
 var cors = require('cors')
 const app = express()
@@ -10,21 +10,17 @@ app.use(express.static('public'))
 app.use(cors())
 
 const port = process.env.PORT || 3000
-
-const things = require('./routes/things')
-
+const router = express.Router()
+bookRoutes = require('./routes/bookRoutes')
 
 // connect Database
-mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true})
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose!'))
-
+// mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true})
+// const db = mongoose.connection
+// db.on('error', error => console.error(error))
+// db.once('open', () => console.log('Connected to Mongoose!'))
 
 app.get('/', (req,res) => {
-    res.send('Special root access')
+    res.json({ data: 'Special root access'})
 })
-
-app.use('/things', things)
-
+app.use('/books', bookRoutes)
 app.listen(port, () => console.log('Server is running on the localhost:'+ port))
