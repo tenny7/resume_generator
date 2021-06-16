f0920673022136257d42bdec3ed4350a2e5b4df9
Ovie Tennyson <atomictenny@gmail.com>
2021-06-16 01:38:26 -0500

    local changes

--- index.js
+++ index.js
@@ -21,12 +21,6 @@ const port = process.env.PORT || 3000
 const router = express.Router()
 bookRoutes = require('./routes/bookRoutes')
 
-// connect Database
-// mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true})
-// const db = mongoose.connection
-// db.on('error', error => console.error(error))
-// db.once('open', () => console.log('Connected to Mongoose!'))
-
 app.get('/', (req,res) => {
     res.json({ data: 'Special root access'})
 })
